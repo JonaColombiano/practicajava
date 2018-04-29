6f5b00345f4ae332ea5edfab019f250fdf1b87ed
JonaColombiano <metalmonsoon.75@gmail.com>
2018-04-28 23:08:39 -0500

    Sobrecarga de constructores

--- src/Empleado/usoEmpleado.java
+++ src/Empleado/usoEmpleado.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Empleado;
 
 /**
@@ -11,11 +7,22 @@ package Empleado;
  */
 public class usoEmpleado {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+     Empleado[] empleado=new Empleado[4];
+     empleado[0]=new Empleado("Jose", 1500,2012,01,02);
+     empleado[1]=new Empleado("Maria", 1500,2012,06,15);
+     empleado[2]=new Empleado("Ivan", 1500,2013,05,07);
+     empleado[3]=new Empleado("Juan");//Se llama al segundo constructor que solo recibe un parametro
+     
+     for(Empleado e: empleado){
+         e.SubeSueldo(5);
+     }
+     
+     for(Empleado x: empleado){
+         System.out.println(x.obtenerNombre()+" "+ x.obtenerSueldo()+" "+x.obtenerFechaContrato());
+     }
     }
+    
     
 }
